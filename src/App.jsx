efff44ddf95a6d746bba3ff28f012a65ad0f7841
shays-og <akshaytiwari26100@gmail.com>
2025-11-03 14:30:42 +0530

    modulating code

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,22 @@ import { Input } from './components/Input'
 
 function App() {
 
+  return <Ageverification/ >
+}
+
+function Ageverification() {
   const [colorEnabled, setColorEnabled] = useState(false);
-  return <div>
-    <Base>
-    <Infotext>Verify Your Age</Infotext>
+  return (<div>
+    <Layout>
+      <Infotext>Verify Your Age</Infotext>
       <div className="mb-8 text-xs">Please confirm your birth year. This data will not be stored.</div>
       <Input type={"text"} placeholder={"Your Birth Year"} setColorEnabled={setColorEnabled}></Input>
       <Button disabled={colorEnabled}>Continue</Button>
-    </Base>
-  </div>
+    </Layout>
+  </div>)
 }
 
-function Base({ children }) {
+function Layout({ children }) {
   return <div className="flex flex-col justify-center items-center bg-prussian-blue h-screen text-white">
     <div className="mb-16 -mt-16 text-xl">
       <span className="text-turquoise">Webinar</span>.gg
