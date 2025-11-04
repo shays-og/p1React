00187fd5e352dccf56e53890611b0a5c65187490
shays-og <akshaytiwari26100@gmail.com>
2025-11-04 23:03:12 +0530

    email page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from "react"
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { Button } from './components/Button'
@@ -19,21 +19,64 @@ function App() {
 function Userdata() {
   const [colorEnabled, setColorEnabled] = useState(false);
   const [continued, setContinued] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+
 
   const buttonOnClick = () => {
     setContinued(true);
-    if (continued){
-
-    };
+    setColorEnabled(false);
+    setUserInput("");
+ 
   }
-  return (<div>
-    <Layout 
-    textHead={"Verify Your Age"} 
-    description={"Please confirm your birth year. This data will not be stored."}>
-      <Input type={"text"} placeholder={"Your Birth Year"} setColorEnabled={setColorEnabled} />
-      <Button onClick={buttonOnClick} disabled={colorEnabled}>Continue</Button>
-    </Layout>
-  </div>)
+
+  const dateCheck = (e) => {
+      const value = e.target.value;
+      setUserInput(value);
+    }
+    useEffect(() => {
+      const timeOut = setTimeout(() => {
+        const date = new Date(userInput);
+        const today = new Date();
+        const ageYears = today.getFullYear() - date.getFullYear();
+        const ageMonth = today.getMonth() - date.getMonth();
+        const ageDay = today.getDay() - date.getDay();
+        if (ageYears > 18 || (ageYears === 18 && ageMonth > 0) || (ageYears === 18 && ageMonth === 0 && ageDay >= 0) ) {
+          console.log("Checking logic for age verification.");
+          setColorEnabled(true);
+        }
+      }, 1000)
+  
+      return () => clearTimeout(timeOut);
+    }, [userInput])
+
+
+    const mailCheck = (e) => {
+      const value = e.target.value;
+      setUserEmail(value);
+      console.log("Valid Mail")
+    }
+
+    if (continued) {
+      return (<div>
+        <Layout 
+        textHead={"Let's Get Started"} 
+        description={""}>
+          <Input type={"text"} placeholder={"EmailId"} userInput={userEmail} validCheck={mailCheck} />
+          <Button onClick={buttonOnClick} disabled={colorEnabled}>Continue</Button>
+        </Layout>
+      </div>)
+    } else {
+      return (<div>
+        <Layout 
+        textHead={"Verify Your Age"} 
+        description={"Please confirm your birth year. This data will not be stored."}>
+          <Input type={"text"} placeholder={"Your Birth Year"} userInput={userInput} validCheck={dateCheck} />
+          <Button onClick={buttonOnClick} disabled={colorEnabled}>Continue</Button>
+        </Layout>
+      </div>)
+    }
+  
 }
 
 export default App
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,38 +1,12 @@
 
-import { useEffect, useState } from "react"
 export function Input({
   type,
   placeholder,
-  setColorEnabled
+  userInput,
+  validCheck
 }) {
 
-  const [userDate, setUserDate] = useState("");
-
-  const dateCheck = (e) => {
-    const value = e.target.value;
-    setUserDate(value);
-    }
-
-  useEffect(() => {
-    const timeOut = setTimeout(() => {
-      const date = new Date(userDate);
-      const today = new Date();
-      const ageYears = today.getFullYear() - date.getFullYear();
-      const ageMonth = today.getMonth() - date.getMonth();
-      const ageDay = today.getDay() - date.getDay();
-      if (ageYears > 18 || (ageYears === 18 && ageMonth > 0) || (ageYears === 18 && ageMonth === 0 && ageDay >= 0) ) {
-        console.log("Checking logic for age verification.");
-        setColorEnabled(true);
-      }
-    }, 2000)
-
-    return () => clearTimeout(timeOut);
-  }, [userDate])
-  
-
-
-
   return <div className="mb-8 -mt-4 ">
-    <input type={type} className="bg-[#18395F] w-64 h-8 rounded-lg outline-none text-sm px-4 placeholder-gray-500/75 placeholder:text-sm" placeholder={placeholder} value={userDate} onChange={dateCheck} />
+    <input type={type} className="bg-[#18395F] w-64 h-8 rounded-lg outline-none text-sm px-4 placeholder-gray-500/75 placeholder:text-sm" placeholder={placeholder} value={userInput} onChange={validCheck} />
   </div>
 }
